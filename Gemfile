fbf70d23d025b808f7560ecd713e1f9ad6c24ab9
Philip27312731 <philip.bernin@elev.ga.ntig.se>
2025-11-27 16:04:48 +0100

    dfghs
    rsthyfstgy

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+gem 'sinatra', '~> 3.0'
+gem 'sqlite3', '~> 1.6'
+gem 'slim', '~> 5.1'
+gem 'sinatra-contrib', '~> 3.0'
+gem 'webrick', '~> 1.8'
--- app.rb
+++ app.rb
@@ -1,86 +1,148 @@
 require 'sinatra'
 require 'sqlite3'
 require 'slim'
-require 'sinatra/reloader'
+require 'sinatra/reloader' if development?
 
+# Konfigurera Sinatra för att hitta statiska filer
+set :public_folder, 'public'
 
+# Databashjälpare
+def db
+  database = SQLite3::Database.new('db/todos.db')
+  database.results_as_hash = true
+  database
+end
 
+# Initialisera databasen
+def init_db
+  database = db
+  database.execute <<-SQL
+    CREATE TABLE IF NOT EXISTS todos (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+      description TEXT,
+      amount INTEGER DEFAULT 0,
+      category TEXT DEFAULT 'privat',
+      completed INTEGER DEFAULT 0,
+      created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    );
+  SQL
+  database.close
+end
 
+# Kör vid start
+begin
+  init_db
+rescue => e
+  puts "Database initialization warning: #{e.message}"
+end
 
-# Routen /
+# Startsida
 get '/' do
-    slim(:index)
+  slim(:index)
 end
 
-post('/todos/:id/delete') do 
-id = params[:id].to_i 
-db = SQLite3::Database.new('db/todos.db')
-db.execute("DELETE FROM todos WHERE id = ?",id)
-redirect('/todos')
+# Visa alla todos
+get '/todos' do
+  database = db
+  
+  query = params[:q]
+  
+  if query && !query.empty?
+    @datatodo = database.execute(
+      "SELECT * FROM todos WHERE name LIKE ? ORDER BY completed ASC, created_at DESC",
+      "%#{query}%"
+    )
+  else
+    @datatodo = database.execute(
+      "SELECT * FROM todos ORDER BY completed ASC, created_at DESC"
+    )
+  end
+  
+  slim(:"todos/index")
 end
 
-post('/todos/:id/update') do 
-  id = params[:id].to_i 
-  name = params[:name]
+# Visa formulär för ny todo
+get '/todos/new' do
+  slim(:"todos/new")
+end
+
+# Skapa ny todo
+post '/todos' do
+  name = params[:new_todo]
+  description = params[:description]
   amount = params[:amount].to_i
-  db = SQLite3::Database.new('db/todos.db')
-  db.execute("UPDATE todos SET name=?,amount=? WHERE id=?", [name,amount,id])
+  category = params[:category] || 'privat'
+  
+  database = db
+  database.execute(
+    "INSERT INTO todos (name, description, amount, category) VALUES (?, ?, ?, ?)",
+    [name, description, amount, category]
+  )
+  
   redirect('/todos')
-
-
-
-
-
 end
 
-get('/todos/:id/edit') do
- db = SQLite3::Database.new('db/todos.db') 
- db.results_as_hash = true
- id = params[:id].to_i
- @special_todo = db.execute("SELECT * FROM todos WHERE id = ?",id).first
- slim(:"todos/edit")
+# Visa redigeringsformulär
+get '/todos/:id/edit' do
+  database = db
+  id = params[:id].to_i
+  
+  @special_todo = database.execute(
+    "SELECT * FROM todos WHERE id = ?",
+    id
+  ).first
+  
+  slim(:"todos/edit")
 end
 
-get('/todos/new') do
- slim(:"todos/new")
+# Uppdatera todo
+post '/todos/:id/update' do
+  id = params[:id].to_i
+  name = params[:name]
+  description = params[:description]
+  amount = params[:amount].to_i
+  category = params[:category] || 'privat'
+  
+  database = db
+  database.execute(
+    "UPDATE todos SET name=?, description=?, amount=?, category=? WHERE id=?",
+    [name, description, amount, category, id]
+  )
+  
+  redirect('/todos')
 end
 
-
-post('/todo') do # app.rb
- new_todo = params[:new_todo] # Hämta datan ifrån formuläret
- amount = params[:amount].to_i 
- db = SQLite3::Database.new('db/todos.db') # koppling till databasen
- db.execute("INSERT INTO todos (name, amount) VALUES (?,?)",[new_todo,amount])
- redirect('/todos') # Hoppa till routen som visar upp alla todoer
+# Markera som färdig/inte färdig
+post '/todos/:id/toggle' do
+  id = params[:id].to_i
+  
+  database = db
+  
+  # Hämta nuvarande status
+  todo = database.execute("SELECT completed FROM todos WHERE id = ?", id).first
+  new_status = todo["completed"] == 1 ? 0 : 1
+  
+  # Uppdatera status
+  database.execute("UPDATE todos SET completed = ? WHERE id = ?", [new_status, id])
+  
+  redirect('/todos')
 end
 
+# Ta bort todo
+post '/todos/:id/delete' do
+  id = params[:id].to_i
+  
+  database = db
+  database.execute("DELETE FROM todos WHERE id = ?", id)
+  
+  redirect('/todos')
+end
 
-
-get('/todos')do
-
-#Gör en koppling till db
-db = SQLite3::Database.new("db/todos.db")
-
-db.results_as_hash = true
-
-#hämta allt från db 
-datatodo = db.execute("SELECT * FROM todos")
-
-
-
-
-querry = params[:q]
-
-if querry && !querry.empty?
-    @datatodo = db.execute("SELECT * FROM todos WHERE name LIKE ?","%#{querry}%")
-
-
-else 
-    @datatodo = db.execute("SELECT * FROM todos")
-
-end 
-
-#visa upp med slim
-slim(:"todos/index")
-
+# Rensa alla färdiga todos
+post '/todos/clear_completed' do
+  database = db
+  database.execute("DELETE FROM todos WHERE completed = 1")
+  
+  redirect('/todos')
 end
